758748213cf0972040b0e464064257c175696439
Hillevi Eklöw <hillevieklow@gmail.com>
2018-12-16 18:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, name){
+  return collection.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  })
+}
+
+function fuzzyMatch(){
+
+}
+
+function matchName(collection, name){
+
+}
