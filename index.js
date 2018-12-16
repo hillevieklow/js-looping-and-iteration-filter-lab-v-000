9d035cf46f349f85ca1a06736b620e9b9d7dba2a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-12-16 18:14:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,22 +2,20 @@
 
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-function findMatching(collection, name){
+function findMatching(collection, name) {
   return collection.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
-  })
+  });
 }
 
-function fuzzyMatch(collection, fuzzyName){
-  let nameLength = fuzzyName.lwngth;
+function fuzzyMatch(collection, fuzzyName) {
+  let nameLength = fuzzyName.length;
   return collection.filter(function (driverName) {
     return driverName.slice(0, nameLength) === fuzzyName
-  })
-
+  });
 }
 
-function matchName(collection, name){
+function matchName (collection, name) {
   return collection.filter(function (driver) {
-    return driver.name.toLowercase() === name.toLowerCase();
-  })
-}
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
